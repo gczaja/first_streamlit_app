ac035db873845a3307db8b57ebd3067f9b4ef9bb
gczaja <gczaja@gmail.com>
2022-05-07 20:19:27 +0200

    Update dev_app.py

--- dev_app.py
+++ dev_app.py
@@ -16,3 +16,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #sdssdsdsdsdsd
 fruits_selected = streamlit.multiselect("Pick some frit:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+#ssdsdsd
